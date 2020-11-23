b7017e7827c05327309d72df77b2eea26df35f29
nameri.hide <ec2-user@ip-172-31-23-152.ec2.internal>
2020-11-23 06:16:54 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_133632) do
+ActiveRecord::Schema.define(version: 2020_11_23_051724) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
