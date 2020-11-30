36a5ee94395f650ab7e674afb078cf3c01dc5882
nameri.hide <ec2-user@ip-172-31-23-152.ec2.internal>
2020-11-30 06:47:36 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/create'
+  root to: 'tasks#index'
+  
+  get 'tasks#index', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
-  resources :tasks
+  resources :tasks, only:[:index, :show, :new, :create]
 end
