971f383ecd52f2bfbd647f934cde83681c09da7b
nameri.hide <ec2-user@ip-172-31-23-152.ec2.internal>
2020-12-11 04:16:17 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :tasks, only: [:index, :show, :new, :create]
+  resources :tasks
   
   resources :users, only: [:index, :show, :new, :create]
 end
