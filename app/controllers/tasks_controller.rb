dfc1c2b8436a71cbd7de6a60275c2e6d152f3bc9
nameri.hide <ec2-user@ip-172-31-23-152.ec2.internal>
2020-12-14 08:23:40 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,8 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:destroy,:show,:update,:edit]
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def show
